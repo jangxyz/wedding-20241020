4018c5cae6026ca2a59d4074a2b24bbbe485eb7f
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2024-08-28 15:32:25 +0900

    add static files

---
+++ static/i/bgm.js
@@ -0,0 +1,131 @@
+
+
+<html>
+<head>
+<title>:::Bojagicard:::</title>
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_preloadImages() { //v3.0
+  var d=document; if (d.images) { if (!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0) { d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+//--> 
+</script>
+</head>
+<link href='../../css/wed_hompy.css' rel='stylesheet' type='text/css'>
+<body leftmargin="0" topmargin="0" valign="center" bgcolor="#828282" >
+<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
+    <tr height=91>
+    <td> 
+  <tr> 
+   <td> 
+  <meta http-equiv='Refresh' content='0;URL=http://bojagicard.com/404error.php?page=/bgm.js?hash=1724820586'> 
+ <br>
+<br>
+<div style="background-image:url(/ecard/images/bg_gray.gif); background-repeat:repeat-x; height:277px; text-align:center;padding-top:121px; ">
+ <a href="http://bojagicard.com"><img src="ecard/images/aaa.gif" border="0" style="cursor:hand;"  onClick="window.open('http://www.bojagicard.com/wed_hom/doc/hompy/index.php?wed_code=', 'newwin','height=700,width=800,scrollbars=no,resizable=no');" ></a>
+</div> 
+   
+   </td>
+   
+   </tr>
+   <tr>
+       <td  height=91 >
+    <table width=100%>
+        <tr>
+            <td width=200>
+    <a href="/?PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04"><img src="/ecard/images/logo.gif" border=0></a>
+    </td>
+    <td>
+    <div style='overflow:hidden; height:90px;'>
+                <a href="/card/doc/product/product_detail.php?cardCode=gb277&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb277_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb285&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb285_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb295&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb295_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb253&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb253_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb217&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb217_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb299&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb299_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb338&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb338_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb265&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb265_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb237&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb237_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb274&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb274_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb261&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb261_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb300&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb300_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb160&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb160_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb148&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb148_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb264&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb264_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb345&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb345_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb118&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb118_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb258&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb258_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb280&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb280_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+                <a href="/card/doc/product/product_detail.php?cardCode=gb301&special=&list_type=&CardType=gb&page=1&add_page=&PHPSESSID=f30a7e2fe854f3df0acdd024c176ff04">
+        <img src="https://img.bojagicard.com/product/gb301_a_1.jpg" height=86 style="border:2px solid #fff; cursor:pointer;" border=0 />
+        </a>
+        
+            
+    </div>
+    
+    </td>
+    </tr>
+    </table>
+</table>
+</body>
+</html>
